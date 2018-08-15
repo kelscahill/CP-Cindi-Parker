b70841fcecde846c7a08f24c1201408db36e85c1
Kelsey Cahill <kcahill@southleft.com>
2018-08-14 21:08:47 -0400

    Added form items

--- wp-content/themes/cindi-parker/resources/views/patterns/01-molecules/components/modal.blade.php
+++ wp-content/themes/cindi-parker/resources/views/patterns/01-molecules/components/modal.blade.php
@@ -8,10 +8,15 @@
       <h1 class="u-color--secondary">Join</h1>
       <h2>The Waitlist!</h2>
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    <form action="https://cindiparker.us18.list-manage.com/subscribe/post?u=fc221697a55e2d974ef69c662&amp;id=27af655dea" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <input type="email" placeholder="Enter your email address" name="EMAIL" class="required email" id="mce-EMAIL"/>
-      <button type="submit" name="subscribe" id="mc-embedded-subscribe">Join</button>
+    <p>Hello friend! Thank you so much for your interest in Cindi Parker Photography. Be the first to get updates on services, tips, & the best encouragement from your favorite dancing goofball!</p>
+    <form class="u-spacing" action="https://cindiparker.us18.list-manage.com/subscribe/post?u=fc221697a55e2d974ef69c662&amp;id=27af655dea" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <input type="text" placeholder="First Name" name="FNAME" id="FNAME" />
+      <input type="email" class="required" placeholder="Email Address" name="EMAIL" id="EMAIL" required />
+      <fieldset>
+        <input type="checkbox" value="I agree to the terms of services" required />
+        <label for="checkbox">I agree to the <a class="u-link--underline" target="_blank" href="http://cindiparker.com/wp-content/uploads/2018/08/CINDIPARKER_Terms-of-Service_08112018.pdf">terms and conditions</a>.</label>
+      </fieldset>
+      <button>Join</button>
     </form>
   </div>
 </div>
